0959778fc4e15e68082a0d5d867f73d9680e2595
jainarchi <imarchijain2405@gmail.com>
2026-02-26 14:59:41 +0530

    done

--- Frontend/src/pages/ManagePassword.jsx
+++ Frontend/src/pages/ManagePassword.jsx
@@ -1,4 +1,3 @@
-import Manager from '../Components/Manager';
 import SavePassword from '../Components/SavePassword';
 
 
@@ -8,7 +7,6 @@ function ManagePassword() {
     <>
      
       <div className=' bg-green-50 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]'>
-      {/* < Manager />  */}
       <SavePassword />
       </div>
      
